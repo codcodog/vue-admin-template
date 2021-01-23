4a772d648d4720eac9a337050893a41d83786a7d
codcodog <codcodog@gmail.com>
2021-01-23 22:03:26 +0800

    fix: monitor type support

--- src/views/stock/monitor.vue
+++ src/views/stock/monitor.vue
@@ -12,12 +12,26 @@
                 <el-radio v-model="formData.status" :label="1">开启</el-radio>
                 <el-radio v-model="formData.status" :label="0">关闭</el-radio>
             </el-form-item>
-            <el-form-item label="BUY PRICE" prop="buyPrice" label-width="105px">
-                <el-input v-model="formData.buyPrice"></el-input>
+            <el-form-item label="类 型" prop="type">
+                <el-radio v-model="formData.type" :label="0">BIAS</el-radio>
+                <el-radio v-model="formData.type" :label="1">PRICE</el-radio>
             </el-form-item>
-            <el-form-item label="SELL PRICE" prop="sellPrice" label-width="105px">
-                <el-input v-model="formData.sellPrice"></el-input>
-            </el-form-item>
+            <div v-if="formData.type==0">
+                <el-form-item label="BUY BIAS" prop="buyBias" label-width="105px">
+                    <el-input v-model="formData.buyBias"></el-input>
+                </el-form-item>
+                <el-form-item label="SELL BIAS" prop="sellBias" label-width="105px">
+                    <el-input v-model="formData.sellBias"></el-input>
+                </el-form-item>
+            </div>
+            <div v-if="formData.type==1">
+                <el-form-item label="BUY PRICE" prop="buyPrice" label-width="105px">
+                    <el-input v-model="formData.buyPrice"></el-input>
+                </el-form-item>
+                <el-form-item label="SELL PRICE" prop="sellPrice" label-width="105px">
+                    <el-input v-model="formData.sellPrice"></el-input>
+                </el-form-item>
+            </div>
             <el-form-item label="备 注" prop="message" label-width="105px">
                 <el-input type="textarea" :autosize="{minRows: 2, maxRows: 5}"  v-model="formData.message"></el-input>
             </el-form-item>
@@ -39,12 +53,18 @@ export default {
             formName: 'monitorForm',
             formData: {
                 status: 1,
+                type: 0,
                 buyPrice: '',
                 sellPrice: '',
+                buyBias: '',
+                sellBias: '',
                 message: '',
             },
             rules: {
                 status: [
+                    {required: true}
+                ],
+                type: [
                     {required: true}
                 ],
                 buyPrice: [
@@ -52,6 +72,12 @@ export default {
                 ],
                 sellPrice: [
                     {required: true, message: "请输入 sell price", trigger: 'blur'}
+                ],
+                buyBias: [
+                    {required: true, message: "请输入 buy bias", trigger: 'blur'}
+                ],
+                sellBias: [
+                    {required: true, message: "请输入 sell bias", trigger: 'blur'}
                 ],
             },
         }
@@ -62,7 +88,10 @@ export default {
                 if (valid) {
                     var params = {
                         code: this.code,
+                        type: this.formData.type,
                         status: this.formData.status,
+                        buy_bias: this.formData.buyBias,
+                        sell_bias: this.formData.sellBias,
                         buy_price: this.formData.buyPrice,
                         sell_price: this.formData.sellPrice,
                         message: this.formData.message,
@@ -83,7 +112,9 @@ export default {
         },
         init: function() {
             if (this.$refs[this.formName] !== undefined) {
+                console.log("====buy price", this.formData.buyPrice)
                 this.$refs[this.formName].resetFields();
+                console.log("====after reset", this.formData.buyPrice)
             }
         },
         // 获取价格监控数据
@@ -101,6 +132,9 @@ export default {
                     this.init()
                 } else {
                     this.formData.status = response.data.status
+                    this.formData.type = response.data.type
+                    this.formData.buyBias = response.data.buy_bias
+                    this.formData.sellBias = response.data.sell_bias
                     this.formData.buyPrice = response.data.buy_price
                     this.formData.sellPrice = response.data.sell_price
                     this.formData.message = response.data.message
@@ -111,8 +145,9 @@ export default {
     watch: {
         showMonitor: function(newValue, oldValue) {
             if (newValue) { // 显示
-                this.init()
                 this.getPriceMonitor()
+            } else {
+                this.init()
             }
         }
     }
